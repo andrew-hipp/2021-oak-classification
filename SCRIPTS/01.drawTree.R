be29ea8b827773c097bf795e5102d6f1a5b4b6a8
andrew-hipp <andrew.hipp@gmail.com>
2021-02-07 14:53:07 -0600

    Pretty tree updates

--- SCRIPTS/01.drawTree.R
+++ SCRIPTS/01.drawTree.R
@@ -1,4 +1,19 @@
 require(ggtree)
 require(ggplot2)
 
-p <- ggtree(tr)
+tr.plot <- tr
+tr.plot$tip.label <- gsub('_', ' ', tr.plot$tip.label, fixed = T)
+p <- ggtree(tr.plot, layout = 'fan', open.angle = 180)
+p <- p + geom_tiplab2(fontface='italic',
+                      size = 1.7
+                      # ,
+                      # color = ifelse(tip.dat[tr$tip.label, 'NAm'], "black", "gray")
+                      )
+
+pdf('../OUT/prettyTree.pdf', 8.5, 11)
+layout(matrix(4:1,4,1))
+# print(type = 'n')
+# print(type = 'n')
+# print(type = 'n')
+print(p)
+dev.off()
