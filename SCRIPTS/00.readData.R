c530c96868f8bcb83779a820ce593c760e1c2021
andrew-hipp <andrew.hipp@gmail.com>
2021-02-07 09:09:35 -0600

    Bringing revised data in; adding 01.drawTree

--- SCRIPTS/00.readData.R
+++ SCRIPTS/00.readData.R
@@ -2,6 +2,10 @@
 
 library(ape)
 library(openxlsx)
+library(magrittr)
+
+## tip.dat.raw = original tip data from Hipp et al. 2020, New Phyt
+## tip.dat = edited from this tree, used for downstream analyses
 
 tr <- read.tree('../DATA/tr.singletons.correlated.1.taxaGrepStem.tre')
 tr <- drop.tip(tr, c("Quercus_arizonica|Mexico|Durango|2015003|QUE001258",
@@ -9,18 +13,20 @@ tr <- drop.tip(tr, c("Quercus_arizonica|Mexico|Durango|2015003|QUE001258",
                      "Quercus_conzattii|Mexico|Oaxaca|JCB-MX-OA-TE-4|QUE000197"))
 
 tr$tip.label <- sapply(strsplit(tr$tip.label, '|', fixed = T), '[', 1)
-tip.dat <- read.xlsx('../DATA/includeTips.2018-10-30-v2.xlsx', 1)
-tip.dat$sp <- sapply(strsplit(tip.dat$tip, '_|_', fixed = T), '[', 1)
-tip.dat <- tip.dat[which(tip.dat$singleTip), ]
+tip.dat.raw <- read.xlsx('../DATA/includeTips.2018-10-30-v2.xlsx', 1)
+tip.dat.raw$sp <- sapply(strsplit(tip.dat.raw$tip, '_|_', fixed = T), '[', 1)
+tip.dat.raw <- tip.dat.raw[which(tip.dat.raw$singleTip), ]
 
-message(paste('Proportion tree tips matching tip.dat:', round(sum(tr$tip.label %in% tip.dat$sp) / length(tr$tip.label), 2)))
+message(paste('Proportion tree tips matching tip.dat.raw:', round(sum(tr$tip.label %in% tip.dat.raw$sp) / length(tr$tip.label), 2)))
 
-tr <- drop.tip(tr, which(!tr$tip.label %in% tip.dat$sp))
-tip.dat <- tip.dat[tip.dat$sp %in% tr$tip.label, ]
-tip.dat <- tip.dat[!duplicated(tip.dat$sp), ]
-tip.dat <- tip.dat[grep('Quercus', tip.dat$subgenus), ]
-tr <- drop.tip(tr, setdiff(tr$tip.label, tip.dat$sp))
+tr <- drop.tip(tr, which(!tr$tip.label %in% tip.dat.raw$sp))
+tip.dat.raw <- tip.dat.raw[tip.dat.raw$sp %in% tr$tip.label, ]
+tip.dat.raw <- tip.dat.raw[!duplicated(tip.dat.raw$sp), ]
+tip.dat.raw <- tip.dat.raw[grep('Quercus', tip.dat.raw$subgenus), ]
+tr <- drop.tip(tr, setdiff(tr$tip.label, tip.dat.raw$sp))
 
 pdf('../OUT/tr.checkingNames.pdf', 8.5,15)
 plot(tr, cex = 0.5)
 dev.off()
+
+tip.dat <- read.csv('../DATA/tips.data.csv', row.names = 7, as.is = TRUE)
---
+++ SCRIPTS/01.drawTree.R
@@ -0,0 +1,2 @@
+require(ggtree)
+require(ggplot2)
